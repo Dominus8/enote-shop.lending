83f708b130236cf53d3899faed9d080b28cf17a7
Dominus8 <ikari162@mail.ru>
2024-01-31 02:28:19 +0300

    add laravel app

---
+++ enote-shop/resources/views/index.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style/normalize.css">
+    <link rel="stylesheet" href="/style/style.css">
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+ 
+    ym(95599995, "init", {
+         clickmap:true,
+         trackLinks:true,
+         accurateTrackBounce:true
+    });
+    </script>
+    
+ <!-- /Yandex.Metrika counter -->
+
+    <title>Купить электронику</title>
+
+</head>
+<body>
+    <div class="container text-center">
+        <div class="row">
+          <div class="col">
+            <div class="row">
+                <nav class="navbar fixed-top bg-secondary-subtle">
+                    <div class="container">
+                        <a target="_blank" class="navbar-brand" href="/">
+                            <img src="/images/logo_header.svg" alt="Logo" width="40" height="40" > <!--class="d-inline-block align-text-top" -->
+                            E-shop
+                          </a>
+                        <div class="contacts ">
+                            <div class="">8-499-350-26-08</div>
+                            <div class="">info@enote-shop.ru</div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+
+            <div class="row content-wrapper">
+                <div class="col-0 left-sitebar">
+
+                </div>
+                <div class="col mine-content">
+                    <div class="top-banner">
+                        <img src="/images/banner.png" alt="">
+                    </div>
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                          <div class="card h-100">
+                            <a target="_blank" href="/hdd-box-2-5.html">
+                                <div class="product-card-image-wrapper">
+                                    <img src="/images/00028/28-7.jpeg" class="card-img-top product-card-image" alt="...">
+                                </div>
+                                
+                            <div class="card-body card-body d-flex flex-column">
+                              <h5 class="card-title p-2">Корпус для жесткого диска HDD SSD (2.5", USB 3.0, SATA)</h5>
+                            </a>
+                              <p class="card-text p-2">Внешний бокс для HDD или SDD (2.5", USB 3.0, SATA). Подключение производится посредством встроенного разъема USB.</p>
+                                <div class="row market-links p-2 mt-auto">
+                                    <div class="col">
+                                        <a target="_blank" href="https://www.wildberries.ru/catalog/97424737/detail.aspx">
+                                            <img class="img-fluid" height="50px" width="50px" src="/images/wb-ico.svg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a target="_blank" href="https://ozon.ru/product/korpus-dlya-zhestkogo-diska-ot-noutbuka-2-5-usb-3-0-vneshniy-boks-dlya-hdd-ssd-sata-hdd-box-471454218/?utm_source=e-shhop&utm_medium=cpc&utm_campaign=vendor_org_145536_{campaign_id}&utm_term={keyword}&utm_content={ad_id}&hs=1">
+                                            <img class="img-fluid" height="50px" width="50px" src="/images/oz-ico.svg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a target="_blank" href="https://market.yandex.ru/product--vneshnii-hdd-ssd-boks-2-5-usb-3-0-sata-vneshnii-korpus-dlia-zhestkogo-diska-perekhodnik-hdd-usb-3-0/1760864959?sku=101789211895&uniqueId=7452361&_redirectCount=1">
+                                            <img class="img-fluid" height="50px" width="50px" src="/images/ym-ico.svg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a target="_blank" href="https://www.avito.ru/moskovskaya_oblast_krasnogorsk/tovary_dlya_kompyutera/korpus_dlya_zhestkogo_diska_2.5_usb_3.0_sata_hddbox_3431273696">
+                                            <img class="img-fluid" height="50px" width="50px"  src="/images/av-ico.svg" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a target="_blank" href="#">
+                                            <img class="img-fluid" height="50px" width="50px" src="/images/es-ico.svg" alt="">
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                          <div class="card h-100">
+                            <a target="_blank" href="wi-fi-adapter-600Mb-s.html">
+                            <img src="/images/00029/wi-fi-adapter-1.webp" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column">
+                              <h5 class="card-title p-2">Wi-Fi адаптер для компьютера сетевой USB, 2,4 и 5 ГГЦ</h5>
+                            </a>  
+                              <p class="card-text p-2">Именно благодаря таким адаптерам удается обмениваться пакетами данных без проводного соединения с сетью Internet.</p> 
+                              <div class="row market-links p-2 mt-auto">
+                                <div class="col">
+                                    <a target="_blank" href="https://www.wildberries.ru/catalog/97755776/detail.aspx">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/wb-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://www.ozon.ru/product/wi-fi-adapter-realtek-600m-338798837/">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/oz-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://market.yandex.ru/product--wi-fi-ac-adapter-usb-600m-802-11ac-2-4-i-5-ggts-windows-linux/1754900730">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/ym-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://www.avito.ru/moskovskaya_oblast_krasnogorsk/tovary_dlya_kompyutera/wifi_ac_adapter_usb_600m_802.11ac_24_i_5_ggts_w_2407807260">
+                                        <img class="img-fluid" height="50px" width="50px"  src="/images/av-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="#">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/es-ico.svg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+                          <div class="card h-100">
+                            <a target="_blank" href="optibay-9,5mm.html">
+                            <img src="/images/00021/optibay-9.5mm-1.webp" class="card-img-top" alt="...">
+                            <div class="card-body d-flex flex-column">
+                              <h5 class="card-title p-2">Адаптер переходник для жесткого диска HDD/SSD, Optibay 9.5мм</h5>
+                              </a>
+                              <p class="card-text p-2">Оптибей 9,5 мм устанавливается внутрь ноутбука на место стандартного оптического привода(CD-ROM/DVD-ROM).</p>
+                              <div class="row market-links p-2 mt-auto">
+                                <div class="col">
+                                    <a target="_blank" href="https://www.wildberries.ru/catalog/97731095/detail.aspx">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/wb-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://www.ozon.ru/product/optibay-12-7-mm-perehodnik-dopolnitelnogo-zhestkogo-diska-v-cd-dvd-otsek-noutbuka-12-7mm-329952524/?avtc=1&avte=2&avts=1699476875">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/oz-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://market.yandex.ru/product--salazki-dlia-noutbuka-9-5-mm-optibay-dlia-hdd-ssd-optibei-adapter-dlia-zhestkogo-diska-2-5-vmesto-diskovoda/1760864958">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/ym-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="https://www.avito.ru/moskovskaya_oblast_krasnogorsk/tovary_dlya_kompyutera/optibay_95mm_perehodnik_dlya_hdd_i_ssd_salazki_2375333703">
+                                        <img class="img-fluid" height="50px" width="50px"  src="/images/av-ico.svg" alt="">
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a target="_blank" href="#">
+                                        <img class="img-fluid" height="50px" width="50px" src="/images/es-ico.svg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="col">
+
+                        </div>
+                      </div>
+                </div>
+            </div>
+            <div class="row">
+                <noscript><div><img src="https://mc.yandex.ru/watch/95599995" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+
+            </div>
+          </div>
+        </div>
+      </div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+<script src="/js/mine.js"></script>
+
+</html>
